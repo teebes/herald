15321950697559b3f0f80ef021c28a6d604d02ff
Thibaud Morel <teebes@gmail.com>
2019-09-19 17:50:56 -0400

    wip quest log modal

--- src/components/game/QuestLog.vue
+++ src/components/game/QuestLog.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div id="quest_log" v-if="log_entries.length">
     <h1>Quest Log</h1>
 
     <div v-for="log_entry in log_entries" :key="log_entry.id" class="mt-4">
-      <h2>{{ log_entry.quest_name }}</h2>
-      <div>Quest given by {{ log_entry.quest_giver }}</div>
-      <div v-for="(line, index) in log_entry.enquire_cmds" :key="index" class="mt-2">{{ line }}</div>
+      <h2 @click="onClickName(log_entry)" class="mb-2">
+        <span class="interactable">{{ log_entry.quest_name }}</span>
+      </h2>
+      <template v-if="expanded == log_entry.id">
+        <div>Quest given by {{ log_entry.quest_giver }}</div>
+        <div v-for="(line, index) in log_entry.enquire_cmds" :key="index" class="mt-2">{{ line }}</div>
+      </template>
     </div>
   </div>
 </template>
@@ -25,15 +29,26 @@ interface QuestLogEntry {
 @Component
 export default class QuestLog extends Vue {
   log_entries: QuestLogEntry[] = [];
+  expanded: number = 0;
 
   async created() {
     const resp = await axios.get("/game/enquiredquests/");
     this.log_entries = resp.data;
-    //console.log(this.log_entries);
+
+    if (this.log_entries.length) {
+      this.expanded = this.log_entries[0].id;
+    }
+  }
+
+  onClickName(log_entry) {
+    this.expanded = log_entry.id;
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "@/styles/colors.scss";
+#quest_log {
+  padding: 15px;
+}
 </style>
