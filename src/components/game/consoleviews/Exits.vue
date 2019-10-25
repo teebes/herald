960f9aa8c8477f0fb8fa571d1ac3275175d2f90f
Thibaud Morel <teebes@gmail.com>
2019-10-25 18:22:37 -0400

    Add room IDs for builders when viewing exits

---
+++ src/components/game/consoleviews/Exits.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    Available exits:
+    <template v-if="Object.keys(message.data).length">
+      <div v-for="(room_data, direction) in message.data" :key="direction">
+        {{ direction }} - {{ room_data.name }}
+        <span
+          v-if="$store.state.game.player.is_immortal"
+          class="color-text-50 ml-4"
+        >[ {{ room_data.id }} ]</span>
+      </div>
+    </template>
+    <div v-else>None.</div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { DIRECTIONS } from "@/constants.ts";
+
+@Component
+export default class Exits extends Vue {
+  @Prop() message!: any;
+
+  mounted() {
+    console.log();
+    console.log("");
+  }
+
+  // get roomExits() {
+  //   const exits = [];
+  //   for (const direction in DIRECTIONS) {
+  //     if (this.message[direction]) {
+  //       exits.push(this.message[direction])
+  //     }
+
+  //   }
+  // }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/colors.scss";
+</style>
