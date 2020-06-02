aaf1a73359c5c5c56d0656ff078600990a76eb95
Thibaud Morel <teebes@gmail.com>
2020-06-02 14:08:26 -0400

    Add move zone screen

--- src/components/builder/zone/ZoneConfig.vue
+++ src/components/builder/zone/ZoneConfig.vue
@@ -10,7 +10,18 @@
 
         <router-link :to="processions_list_link">manage</router-link>
       </div>
+
+      <div class="move">
+        <h3>MOVE ZONE</h3>
+
+        <div>This action moves all rooms in a zone at once, given a certain direction and distance.</div>
+
+        <button class="btn-small mt-2" @click="moveZone">MOVE</button>
+
+        <div></div>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -18,6 +29,7 @@
 import { Component, Prop, Vue, Watch, Mixins } from "vue-property-decorator";
 import ZoneView from "@/components/builder/zone/ZoneView";
 import { BUILDER_ZONE_PROCESSION_LIST } from "@/router";
+import { FormElement, DIRECTION } from "@/core/forms";
 
 @Component
 export default class ZoneConfig extends Mixins(ZoneView) {
@@ -26,6 +38,26 @@ export default class ZoneConfig extends Mixins(ZoneView) {
       name: BUILDER_ZONE_PROCESSION_LIST,
       params: { zone_id: this.$route.params.zone_id }
     };
+  }
+
+  async moveZone() {
+    const schema: FormElement[] = [
+      DIRECTION,
+      {
+        attr: "distance",
+        label: "Distance"
+      }
+    ];
+    const modal = {
+      title: `Move Zone`,
+      data: {
+        direction: "north",
+        distance: 0
+      },
+      schema: schema,
+      action: "builder/zones/move_zone"
+    };
+    this.$store.commit("ui/modal_set", modal);
   }
 }
 </script>
--- src/components/game/consoleviews/Help.vue
+++ src/components/game/consoleviews/Help.vue
@@ -255,6 +255,7 @@
             <div class="cmd" @click="cmdHelp('ping')">ping</div>
             <div class="cmd" @click="cmdHelp('jump')">jump</div>
             <div class="cmd" @click="cmdHelp('transfer')">transfer</div>
+            <div class="cmd" @click="cmdHelp('find')">find</div>
           </div>
           <div class="cmds">
             <div class="cmd" @click="cmdHelp('echo')">echo</div>
