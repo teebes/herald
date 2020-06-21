58eb4f9efcafa393487d4adf9537a70e3e00f4af
Thibaud Morel <teebes@gmail.com>
2020-06-21 11:27:45 -0400

    Look char: fix to non-binary gender display.

--- src/components/game/CharInfo.vue
+++ src/components/game/CharInfo.vue
@@ -2,26 +2,33 @@
   <div>
     <div class="name">{{ this.$capfirst(char.name) }}</div>
     <div class="summary">
-      Level {{ char.level }} {{ char.gender }} {{ char.archetype }}
+      Level {{ char.level }} {{ gender }} {{ char.archetype }}
       <template v-if="char.is_elite">
         &nbsp;[&nbsp;
         <span class="color-primary">elite</span>&nbsp;]
       </template>
     </div>
-    <div class="health color-text-50">Health: {{ char.health }} / {{ char.health_max }}</div>
-    <div class="description" v-if="char.description">{{ char.description }}</div>
-  
+    <div class="health color-text-50">
+      Health: {{ char.health }} / {{ char.health_max }}
+    </div>
+    <div class="description" v-if="char.description">
+      {{ char.description }}
+    </div>
+
     <div class="equipment" v-if="slots && slots.length">
       <div>{{ this.$capfirst(char.name) }} is using:</div>
       <div v-for="(slot, index) in slots" :key="index">
-        <span class="font-text-regular">{{ slot.slotName }}</span>:
-        <span :class="{ ['color-secondary']: slot.slotItemIsMagic }">{{ slot.slotItemName }}</span>
+        <span class="font-text-regular">{{ slot.slotName }}</span
+        >:
+        <span :class="{ ['color-secondary']: slot.slotItemIsMagic }">{{
+          slot.slotItemName
+        }}</span>
       </div>
     </div>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import { EQUIPMENT_SLOT_LIST } from "@/constants";
 import { capfirst } from "@/core/utils.ts";
@@ -40,11 +47,16 @@ export default class CharInfo extends Vue {
         slots.push({
           slotName: capfirst(slot),
           slotItemName: eq[slot].name,
-          slotItemIsMagic: eq[slot].is_magic
+          slotItemIsMagic: eq[slot].is_magic,
         });
       }
     }
     return slots;
+  }
+
+  get gender() {
+    if (this.char.gender == "non_binary") return "non-binary";
+    return this.char.gender;
   }
 }
 </script>
