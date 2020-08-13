bbca97174a91b1534c85c9ead20d969b4f72faa9
Thibaud Morel <teebes@gmail.com>
2020-08-13 18:40:28 -0400

    Beta orc access to 2 users

--- src/components/lobby/CreateChar.vue
+++ src/components/lobby/CreateChar.vue
@@ -144,7 +144,12 @@ export default class extends Vue {
   }
 
   get showFactions() {
-    if (this.world.id == 1 && this.$store.state.auth.user.is_staff) {
+    if (
+      this.world.id == 1 &&
+      (this.$store.state.auth.user.is_staff ||
+        this.$store.state.auth.user.id === "11216" ||
+        this.$store.state.auth.user.id === "688")
+    ) {
       return true;
     }
     return this.world.core_factions.length && this.world.can_select_faction;
