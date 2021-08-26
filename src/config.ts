f2fb72975de44e9504ec32071fa527bd17c3a031
Mike DuRussel <heyitsmdr@gmail.com>
2021-08-25 22:21:54 -0400

    Use the proper API/WS endpoints depending on the mode of the app

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,5 @@
-export const API_BASE = "https://api.writtenrealms.com/api/v1/";
-// Comment line above and uncomment line below to point to local backend
-// export const API_BASE = "http://localhost:8000/api/v1/";
-
-export const WS_URI = "wss://writtenrealms.com/socket";
-// Comment line above and uncomment line below to point to local backend
-// export const WS_URI = "ws://localhost:8888/cmd";
+export const API_BASE = process.env.VUE_APP_WREALMS_API_ENDPOINT;
+export const WS_URI = process.env.VUE_APP_WREALMS_WS_ENDPOINT
 
 export const MAP_CONFIG = {
   UNIT: 8
