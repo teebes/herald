9be2017e7f24fb9a5417e8bf2c23b15395640818
Thibaud Morel <teebes@gmail.com>
2023-11-19 11:31:04 -0700

    Add review description display for staff.

--- src/components/staff/Reviews.vue
+++ src/components/staff/Reviews.vue
@@ -5,7 +5,7 @@
   <div class="review-list">
     <h2>MY REVIEWS</h2>
     <div class="review mt-4"
-         v-for="review in user_reviews" 
+         v-for="review in user_reviews"
          :key="review.id">
       <div class="world-link mb-4">
         <router-link :to="{ name: 'builder_world_index', params: { world_id: review.world.id } }">{{ review.world.name }}</router-link>
@@ -16,7 +16,10 @@
         <div>{{ review.text }}</div>
       </div>
       <div v-else class="color-text-50">No review written.</div>
-      
+      <div class="review-description mt-4">
+          {{ review.description }}
+        </div>
+
       <div class="actions mt-4"></div>
       <button class="btn-small mr-2" @click="edit_review(review)">EDIT</button>
       <button class="btn-small mr-2" @click="approve_review(review)">APPROVE</button>
@@ -30,15 +33,18 @@
     <div class="review-list unassigned-reviews">
       <h2>UNASSIGNED REVIEWS</h2>
       <div class="review mt-4"
-          v-for="review in unassigned_reviews" 
+          v-for="review in unassigned_reviews"
           :key="review.id">
-        
+
         <div class="world-link">
           <router-link :to="{ name: 'builder_world_index', params: { world_id: review.world.id } }">{{ review.world.name }}</router-link>
         </div>
+        <div class="review-description mt-4">
+          {{ review.description }}
+        </div>
 
         <button class="btn-small mt-4" @click="claim_review(review)">CLAIM</button>
-        
+
       </div>
       <div v-if="unassigned_reviews.length === 0" class="review mt-4">No unassigned reviews.</div>
     </div>
@@ -58,11 +64,11 @@ export default class WorldFrame extends Vue {
     return this.$store.state.staff.reviews.user_reviews;
   }
 
-  get unassigned_reviews() { 
+  get unassigned_reviews() {
     return this.$store.state.staff.reviews.unassigned_reviews;
   }
 
-  async mounted() { 
+  async mounted() {
     this.load_endpoints();
   }
 
@@ -84,7 +90,7 @@ export default class WorldFrame extends Vue {
       default: ""
     };
     const schema = [review_field];
-    
+
     this.$store.commit(UI_MUTATIONS.MODAL_SET, {
       title: "Edit Review",
       data: review,
