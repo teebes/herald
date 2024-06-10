c2abf307ae4f51fcafa41ad6b62068ccc5d397a8
Thibaud Morel <teebes@gmail.com>
2024-06-10 09:08:21 -0600

    Desktop menu icon styling improvements.

--- src/components/game/desktop/Top.vue
+++ src/components/game/desktop/Top.vue
@@ -6,7 +6,13 @@
         <span>{{ world.name }}</span>
       </div>
       <div class="game-menu">
-        <button type="button" class="btn btn-small" @click.stop="onClickMenu">&#9776;</button>
+        <button type="button" class="btn btn-small menu-button" @click.stop="onClickMenu">
+          <div class="menu-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
       </div>
     </div>
     <Menu v-if="showMenu" @close="onClickMenu" />
@@ -28,3 +34,29 @@ const onClickMenu = () => {
   showMenu.value = !showMenu.value;
 }
 </script>
+
+<style scoped lang='scss'>
+@import "@/styles/colors.scss";
+
+.menu-button {
+  background: none;
+  border: 1px solid $color-primary;
+  display: flex;
+  align-items: center;
+
+  .menu-icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 12px;
+  height: 12px;
+
+    span {
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: $color-primary;
+    }
+  }
+}
+</style>
