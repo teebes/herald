5483cf9f4991c3bb14f9ed7b2c91fe6270de1276
Thibaud Morel <teebes@gmail.com>
2024-07-16 14:50:49 -0600

    Change review submission verbiage.

--- src/components/builder/world/ReviewInstructions.vue
+++ src/components/builder/world/ReviewInstructions.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-4">
     <div>
-      Once per month, you can request for your world to be reviewed for publication. Please review the <a href='https://docs.writtenrealms.com/building/worlds/publishing'>World Publishing Guidelines</a> and provide a description for your world below.</div>
+      Once per month, you can request for your world to be reviewed for publication. Submitted worlds should be polished and ready for prime time. Please review the <a href='https://docs.writtenrealms.com/building/worlds/publishing'>World Publishing Guidelines</a> and provide a description for your world below.</div>
 
       <div class="mt-4">You can describe your world however you'd like, but if you're looking for ideas, try to answer some of the following questions in it:<br/>
         <ul class="ml-4">
@@ -9,7 +9,7 @@
           <li>Does your world tell a particular story, or is it a sandbox? Is there an endgame loop?</li>
           <li>Do you have a particular audience in mind?</li>
           <li>What does your world do well? What are its selling points?</li>
-          <li>Is the world in a finished state? Or do you plan to develop it further?</li>
+          <li>Do you have future plans for further development?</li>
         </ul>
       </div>
 
