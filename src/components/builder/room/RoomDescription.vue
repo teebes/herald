d7abbc38721b618ab7742411f2febff129034c9a
Thibaud Morel <teebes@gmail.com>
2024-11-13 08:54:29 -0700

    Fix room details highlight bug in editor.

--- src/components/builder/room/RoomDescription.vue
+++ src/components/builder/room/RoomDescription.vue
@@ -24,19 +24,24 @@ const isDetail = (word: string) => {
   const trimmedWord = word.split(/\W+/)[0];
   if (!props.room.details) return false;
   for (const detail of props.room.details) {
-    if (trimmedWord.toLowerCase() === detail.toLowerCase()) return true;
+    if (trimmedWord.toLowerCase() === detail.toLowerCase()) {
+      return true;
+    }
   }
   return false;
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/styles/fonts.scss";
 @import "@/styles/colors.scss";
 .description-content {
   .description-line {
     min-height: 14px;
 
     span.interactive {
+      @include font-text-regular;
+      border-bottom: 1px dotted #888;
       cursor: default;
     }
   }
